83446f8f3e0e8e7680f4a9efdc7c14ad36ce2c5c
maharavo11 <maharavo@aims.ac.za>
2025-02-09 17:29:02 +0300

    Update importing_package.R

--- R/importing_package.R
+++ R/importing_package.R
@@ -13,10 +13,18 @@ library(car)
 #data manipulation
 library(tidyr)
 library(dplyr)
+library(mice)
 
 # spatial data
 library(sf)
-
 library(lubridate)
-library(mice)
+
 library(mgcv)
+
+library(xgboost)
+library(SHAPforxgboost)
+library(kknn)
+library(tfruns)
+library(FNN)
+
+library(reshape2)
