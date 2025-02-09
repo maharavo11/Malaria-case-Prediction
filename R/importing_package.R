b06526078812d5723541f4975a8c11affb564c11
unknown <maharavo@aims.ac.za>
2025-02-09 13:04:55 +0300

    Added a new folder and an R script

---
+++ R/importing_package.R
@@ -0,0 +1,22 @@
+### USEFUL IMPORTS ###
+
+# directory management
+library(here)
+
+#visualization and exploration
+library(ggplot2)
+library(skimr)
+
+#statistics
+library(car)
+
+#data manipulation
+library(tidyr)
+library(dplyr)
+
+# spatial data
+library(sf)
+
+library(lubridate)
+library(mice)
+library(mgcv)
